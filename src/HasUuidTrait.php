96feae641ee1e3d9245516b4f5b33a05e6b4231c
Šarūnas Norkus <sarunas.norkus@nordsec.com>
2022-08-04 14:15:23 +0300

    Cleanup and allow use latest eloquent version

--- src/HasUuidTrait.php
+++ src/HasUuidTrait.php
@@ -1,56 +1,39 @@
 <?php
+
+declare(strict_types=1);
+
 namespace JamesMills\Uuid;
 
-use JamesMills\Uuid\Exception\MissingUuidColumnException;
 use Ramsey\Uuid\Uuid;
+use Illuminate\Database\Eloquent\Builder;
 
 trait HasUuidTrait
 {
-    protected static function bootHasUuidTrait()
+    protected static function bootHasUuidTrait(): void
     {
         static::creating(function ($model) {
-
             if (!$model->uuid) {
                 $model->uuid = (string)Uuid::uuid4();
             }
         });
     }
 
-    public static function findByUuidOrFail($uuid)
+    public static function findByUuidOrFail(string $uuid): Builder
     {
         return self::whereUuid($uuid)->firstOrFail();
     }
 
-    /**
-     * Eloquent scope to look for a given UUID
-     * 
-     * @param  \Illuminate\Database\Eloquent\Builder $query
-     * @param  String                                $uuid  The UUID to search for
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeWithUuid($query, $uuid)
+    public function scopeWithUuid(Builder $query, string $uuid): Builder
     {
         return $query->where('uuid', $uuid);
     }
 
-    /**
-     * Eloquent scope to look for multiple given UUIDs
-     * 
-     * @param  \Illuminate\Database\Eloquent\Builder $query
-     * @param  Array                                 $uuids  The UUIDs to search for
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeWithUuids($query, Array $uuids)
+    public function scopeWithUuids(Builder $query, array $uuids): Builder
     {
         return $query->whereIn('uuid', $uuids);
     }
 
-    /**
-     * Get the route key for the model.
-     *
-     * @return string
-     */
-    public function getRouteKeyName()
+    public function getRouteKeyName(): string
     {
         return 'uuid';
     }
